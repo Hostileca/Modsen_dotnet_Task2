a36fb43be866310cae988d09cf7286e746e7174c
Kirill Anisko <kirill.anisko13@gmail.com>
2024-06-26 15:25:18 +0300

    fix id visibility

--- BusinessLogicLayer/Dtos/Roles/RoleReadDto.cs
+++ BusinessLogicLayer/Dtos/Roles/RoleReadDto.cs
@@ -2,7 +2,6 @@
 {
     public class RoleReadDto
     {
-        public Guid Id { get; set; }
         public string Name { get; set; }
     }
 }
--- BusinessLogicLayer/Dtos/Users/UserReadDto.cs
+++ BusinessLogicLayer/Dtos/Users/UserReadDto.cs
@@ -4,7 +4,6 @@ namespace BusinessLogicLayer.Dtos.Users
 {
     public class UserReadDto
     {
-        public Guid Id { get; set; }
         public string UserName { get; set; }
         public RoleReadDto Role { get; set; }
     }
